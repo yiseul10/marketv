0b70710407868c39f37befbbd88e0e93d4b06848
yiseul10 <60907634+yiseul10@users.noreply.github.com>
2022-03-02 04:02:46 +0900

    게시판리스트

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -4,24 +4,51 @@ import client from '@libs/server/client';
 import { withApiSession } from '@libs/server/withSession';
 
 async function handler(req: NextApiRequest, res: NextApiResponse<Response>) {
-  const {
-    body: { question },
-    session: { user }
-  } = req;
-  const post = await client.post.create({
-    data: {
-      question,
-      user: {
-        connect: {
-          id: user?.id
+  if (req.method === 'GET') {
+    const posts = await client.post.findMany({
+      include: {
+        user: {
+          select: {
+            name: true,
+            avatar: true,
+            id: true
+          }
+        },
+        _count: {
+          select: {
+            interest: true,
+            answers: true
+          }
         }
       }
-    }
-  });
-  res.json({
-    ok: true,
-    post
-  });
+    });
+    res.json({
+      ok: true,
+      posts
+    });
+  }
+  if (req.method === 'POST') {
+    const {
+      body: { question },
+      session: { user }
+    } = req;
+    const post = await client.post.create({
+      data: {
+        question,
+        user: {
+          connect: {
+            id: user?.id
+          }
+        }
+      }
+    });
+    res.json({
+      ok: true,
+      post
+    });
+  }
 }
 
-export default withApiSession(withHandler({ methods: ['POST'], handler }));
+export default withApiSession(
+  withHandler({ methods: ['GET', 'POST'], handler })
+);
--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -2,23 +2,40 @@ import type { NextPage } from 'next';
 import CircleBtn from '@components/circleBtn';
 import Layout from '@components/layout';
 import Link from 'next/link';
+import useSWR from 'swr';
+import { Post, User } from '@prisma/client';
+
+interface PostWith extends Post {
+  user: User;
+  _count: {
+    interest: number;
+    answer: number;
+  };
+}
+
+interface Posts {
+  ok: boolean;
+  posts: PostWith[];
+}
 
 const Community: NextPage = () => {
+  const { data, error } = useSWR<Posts>('/api/posts');
+  console.log(data);
   return (
     <Layout title='게시판'>
       <div className='divide-y-[1px]'>
-        {[1, 1, 1].map((_, i) => (
-          <Link key={i} href={`/community/`} passHref>
+        {data?.posts?.map(post => (
+          <Link key={post.id} href={`/community/${post.id}`} passHref>
             <a className='flex cursor-pointer flex-col items-start'>
-              <span className='m-4 inline-flex items-center bg-teal-500 px-1 text-xs font-medium'>
+              <span className='m-4 inline-flex items-center bg-teal-300 px-1 text-xs font-medium'>
                 질문있어요
               </span>
               <span className='font-medium px-4 hover:opacity-70'>
-                Q. 맛있는 빵집 추천 좀요?
+                Q. {post.question}
               </span>
               <div className='mt-4 px-4 text-stone-400 text-xs font-medium space-x-1'>
-                <span>자몽이</span>
-                <span>2시간전</span>
+                <span>{post.user.name}</span>
+                <span>{post.createdAt}</span>
               </div>
               <div className='flex space-x-5 px-4 py-3 text-sm text-stone-600'>
                 <span className='flex items-center space-x-2'>
@@ -36,7 +53,7 @@ const Community: NextPage = () => {
                       d='M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z'
                     ></path>
                   </svg>
-                  <span>궁금해요 1</span>
+                  <span>궁금해요 {post?._count?.interest}</span>
                 </span>
                 <span className='flex items-center space-x-2'>
                   <svg
@@ -53,7 +70,7 @@ const Community: NextPage = () => {
                       d='M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z'
                     ></path>
                   </svg>
-                  <span>답변 1</span>
+                  <span>답변 {post?._count?.answer}</span>
                 </span>
               </div>
             </a>
