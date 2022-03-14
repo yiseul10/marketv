08ce6336d9d7045e39ed8d8f377043cb14e76690
yiseul10 <60907634+yiseul10@users.noreply.github.com>
2022-03-15 02:20:34 +0900

    post UI

--- pages/community/[id].tsx
+++ pages/community/[id].tsx
@@ -118,7 +118,7 @@ const Board: NextPage = () => {
                 <svg
                   className='h-4 w-4'
                   fill='none'
-                  stroke={data?.interesting ? 'orange' : 'currentColor'}
+                  stroke='currentColor'
                   viewBox='0 0 24 24'
                   xmlns='http://www.w3.org/2000/svg'
                 >
--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -39,20 +39,36 @@ const Community: NextPage = () => {
               </div>
               <div className='flex space-x-5 px-4 py-3 text-sm text-stone-600'>
                 <span className='flex items-center space-x-2'>
-                  <svg
-                    className='h-4 w-4'
-                    fill='none'
-                    stroke='currentColor'
-                    viewBox='0 0 24 24'
-                    xmlns='http://www.w3.org/2000/svg'
-                  >
-                    <path
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                      strokeWidth='2'
-                      d='M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z'
-                    ></path>
-                  </svg>
+                  {post._count.interest ? (
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      className='h-4 w-4'
+                      viewBox='0 0 20 20'
+                      fill='currentColor'
+                    >
+                      <path
+                        fillRule='evenodd'
+                        d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z'
+                        clipRule='evenodd'
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      className='h-4 w-4'
+                      fill='none'
+                      stroke='currentColor'
+                      viewBox='0 0 24 24'
+                      xmlns='http://www.w3.org/2000/svg'
+                    >
+                      <path
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        strokeWidth='2'
+                        d='M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z'
+                      ></path>
+                    </svg>
+                  )}
+
                   <span>궁금해요 {post?._count?.interest}</span>
                 </span>
                 <span className='flex items-center space-x-2'>
@@ -67,7 +83,7 @@ const Community: NextPage = () => {
                       strokeLinecap='round'
                       strokeLinejoin='round'
                       strokeWidth='2'
-                      d='M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z'
+                      d='M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z'
                     ></path>
                   </svg>
                   <span>답변 {post?._count?.answers}</span>
